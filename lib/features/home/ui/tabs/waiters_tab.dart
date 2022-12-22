c98eb17567f5bae2725974d89fb256c03cf26200
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-21 19:27:16 -0500

    Added waiters tab UI

--- lib/features/home/ui/tabs/waiters_tab.dart
+++ lib/features/home/ui/tabs/waiters_tab.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oyt_admin/features/home/ui/widgets/tab_header.dart';
+import 'package:oyt_front_widgets/buttons/add_button.dart';
 
-class WaitersTab extends ConsumerWidget {
+class WaitersTab extends ConsumerStatefulWidget {
   const WaitersTab({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Text('WaitersTab');
+  ConsumerState<WaitersTab> createState() => _WaitersTabState();
+}
+
+class _WaitersTabState extends ConsumerState<WaitersTab> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const TabHeader(
+          title: 'Meseros',
+          subtitle:
+              'Aca puedes ver los meseros del restaurante, editar los meseros, eliminar meseros y agregar nuevos meseros.',
+        ),
+        AddButton(onTap: _onAddWaiter, text: 'Agregar mesero'),
+        const Divider(),
+        Expanded(
+          child: Scrollbar(
+            controller: _scrollController,
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    onTap: () => _onTapWaiter(),
+                    title: Text('Mesero $index'),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
+
+  void _onAddWaiter() {}
+
+  void _onTapWaiter() {}
 }
