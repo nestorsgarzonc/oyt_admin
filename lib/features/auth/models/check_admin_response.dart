0943aedcc72f9fb3897f27532905d6c5fad683ad
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-24 00:15:58 -0500

    Added linter

--- lib/features/auth/models/check_admin_response.dart
+++ lib/features/auth/models/check_admin_response.dart
@@ -3,6 +3,14 @@ import 'dart:convert';
 import 'package:equatable/equatable.dart';
 
 class CheckAdminResponse extends Equatable {
+  factory CheckAdminResponse.fromJson(String source) =>
+      CheckAdminResponse.fromMap(json.decode(source));
+
+  factory CheckAdminResponse.fromMap(Map<String, dynamic> map) {
+    return CheckAdminResponse(
+      restaurantId: map['restaurantId'] ?? '',
+    );
+  }
   const CheckAdminResponse({
     required this.restaurantId,
   });
@@ -23,16 +31,7 @@ class CheckAdminResponse extends Equatable {
     };
   }
 
-  factory CheckAdminResponse.fromMap(Map<String, dynamic> map) {
-    return CheckAdminResponse(
-      restaurantId: map['restaurantId'] ?? '',
-    );
-  }
-
   String toJson() => json.encode(toMap());
-
-  factory CheckAdminResponse.fromJson(String source) =>
-      CheckAdminResponse.fromMap(json.decode(source));
 
   @override
   String toString() => 'CheckAdminResponse(restaurantId: $restaurantId)';
