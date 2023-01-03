9ede5d38d778baecb95951ae56623ff19dd298a4
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-02 21:24:06 -0500

    Addded more logs

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,13 @@
 echo "Deploying to production..."
 echo "Started switching pubspect files..."
+echo "actual folders"
+ls
 rm pubspec.yaml
 rm pubspec.lock
 rm -rf build
 mv pubspec_deploy.yaml pubspec.yaml
+echo "final folders"
+ls
+echo "content"
+tail -n 20 pubspec.yaml
 echo "Finished switching pubspect files..."
