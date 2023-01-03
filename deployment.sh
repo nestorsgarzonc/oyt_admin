6ed9072ee3a0012472b88910015b567ac9b0ece7
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-02 21:19:00 -0500

    Fix deployment file

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,7 @@
+echo "Deploying to production..."
+echo "Started switching pubspect files..."
 rm pubspec.yaml
 rm pubspec.lock
 rm -rf build
 mv pubspec_deploy.yaml pubspec.yaml
+echo "Finished switching pubspect files..."
