5e7377f9fbf7d264a1b48b3b6d0d77c54bbde421
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-04 16:21:53 -0500

    Using drawer widget

--- lib/features/tables/ui/table_screen.dart
+++ lib/features/tables/ui/table_screen.dart
@@ -9,6 +9,7 @@ import 'package:oyt_front_table/models/tables_socket_response.dart';
 import 'package:oyt_front_table/models/users_table.dart';
 import 'package:oyt_front_table/widgets/call_to_waiter_card.dart';
 import 'package:oyt_front_table/widgets/table_status_card.dart';
+import 'package:oyt_front_widgets/drawer/drawer_layout.dart';
 import 'package:oyt_front_widgets/title/section_title.dart';
 import 'package:oyt_front_widgets/widgets/buttons/custom_elevated_button.dart';
 import 'package:oyt_front_widgets/widgets/custom_text_field.dart';
@@ -29,7 +30,6 @@ class TableScreen extends ConsumerStatefulWidget {
 
 class _TableScreenState extends ConsumerState<TableScreen> {
   final _name = TextEditingController();
-  //final _id = TextEditingController();
   final _qrKey = GlobalKey();
   final _drawerController = ScrollController();
   final _bodyController = ScrollController();
@@ -37,115 +37,101 @@ class _TableScreenState extends ConsumerState<TableScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Container(
-            width: 320,
-            padding: const EdgeInsets.all(15),
-            decoration: CustomTheme.drawerBoxDecoration,
-            child: Scrollbar(
-              controller: _drawerController,
-              child: ListView(
-                controller: _drawerController,
+      body: DrawerLayout(
+        drawerWidth: 320,
+        drawerChild: Scrollbar(
+          controller: _drawerController,
+          child: ListView(
+            controller: _drawerController,
+            children: [
+              const SafeArea(bottom: false, child: SizedBox.shrink()),
+              Row(
                 children: [
-                  const SafeArea(bottom: false, child: SizedBox.shrink()),
-                  Row(
+                  const BackButton(),
+                  Expanded(
+                    child: Text(
+                      'Mesa ${widget.id}',
+                      style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+              const SectionTitle(title: 'Nombre de la mesa'),
+              CustomTextField(label: 'Nombre de la mesa', controller: _name),
+              const SectionTitle(title: 'Codigo QR de la mesa'),
+              RepaintBoundary(
+                key: _qrKey,
+                child: Container(
+                  decoration: CustomTheme.drawerBoxDecoration.copyWith(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black12),
+                  ),
+                  padding: const EdgeInsets.all(5),
+                  child: Column(
                     children: [
-                      const BackButton(),
-                      Expanded(
-                        child: Text(
-                          'Mesa ${widget.id}',
-                          style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-                        ),
+                      QrImage(data: widget.id),
+                      const Text(
+                        'Takuma',
+                        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
                       ),
+                      const SizedBox(height: 2),
+                      Text('Mesa ${widget.id}'),
+                      const SizedBox(height: 5),
                     ],
                   ),
-                  const SectionTitle(title: 'Nombre de la mesa'),
-                  CustomTextField(label: 'Nombre de la mesa', controller: _name),
-                  // const SectionTitle(title: 'Id de la mesa'),
-                  // CustomTextField(
-                  //   label: 'Id de la mesa',
-                  //   controller: _id,
-                  //   enabled: false,
-                  // ),
-                  const SectionTitle(title: 'Codigo QR de la mesa'),
-                  RepaintBoundary(
-                    key: _qrKey,
-                    child: Container(
-                      decoration: CustomTheme.drawerBoxDecoration.copyWith(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.black12),
-                      ),
-                      padding: const EdgeInsets.all(5),
-                      child: Column(
-                        children: [
-                          QrImage(data: widget.id),
-                          const Text(
-                            'Takuma',
-                            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
-                          ),
-                          const SizedBox(height: 2),
-                          Text('Mesa ${widget.id}'),
-                          const SizedBox(height: 5),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  OutlinedButton.icon(
-                    onPressed: _onDownloadQr,
-                    icon: const Icon(Icons.download),
-                    label: const Text('Descargar codigo QR'),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(CustomTheme.redColor),
-                    ),
-                    onPressed: _onDelete,
-                    icon: const Icon(Icons.delete),
-                    label: const Text('Eliminar mesa'),
-                  ),
-                  const SizedBox(height: 5),
-                  ElevatedButton.icon(
-                    onPressed: _onUpdate,
-                    icon: const Icon(Icons.update),
-                    label: const Text('Actualizar mesa'),
-                  ),
-                  const SafeArea(bottom: false, child: SizedBox.shrink()),
-                ],
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            child: Scrollbar(
-              controller: _bodyController,
-              child: ListView(
-                padding: CustomTheme.drawerBodyPadding,
-                controller: _bodyController,
-                children: [
-                  //TODO: Add table status
-                  const SafeArea(bottom: false, child: SizedBox.shrink()),
-                  const TableStatusCard(tableStatus: TableStatus.confirmOrder),
-                  const SizedBox(height: 10),
-                  CallToWaiterCard(
-                    needWaiter: false,
-                    onCallWaiter: (isCalling) {},
-                  ),
-                  const SizedBox(height: 10),
-                  //TODO: Show user list
-                  const Text('User list here'),
-                  const SizedBox(height: 10),
-                  CustomElevatedButton(
-                    onPressed: _onChangeStatus,
-                    child: const Text('Cambiar estado de la mesa'),
-                  ),
-                  const SafeArea(bottom: false, child: SizedBox.shrink()),
-                ],
+              const SizedBox(height: 10),
+              OutlinedButton.icon(
+                onPressed: _onDownloadQr,
+                icon: const Icon(Icons.download),
+                label: const Text('Descargar codigo QR'),
               ),
-            ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(CustomTheme.redColor),
+                ),
+                onPressed: _onDelete,
+                icon: const Icon(Icons.delete),
+                label: const Text('Eliminar mesa'),
+              ),
+              const SizedBox(height: 5),
+              ElevatedButton.icon(
+                onPressed: _onUpdate,
+                icon: const Icon(Icons.update),
+                label: const Text('Actualizar mesa'),
+              ),
+              const SafeArea(bottom: false, child: SizedBox.shrink()),
+            ],
           ),
-        ],
+        ),
+        bodyChild: Scrollbar(
+          controller: _bodyController,
+          child: ListView(
+            padding: CustomTheme.drawerBodyPadding,
+            controller: _bodyController,
+            children: [
+              //TODO: Add table status
+              const SafeArea(bottom: false, child: SizedBox.shrink()),
+              const TableStatusCard(tableStatus: TableStatus.confirmOrder),
+              const SizedBox(height: 10),
+              CallToWaiterCard(
+                needWaiter: false,
+                onCallWaiter: (isCalling) {},
+              ),
+              const SizedBox(height: 10),
+              //TODO: Show user list
+              const Text('User list here'),
+              const SizedBox(height: 10),
+              CustomElevatedButton(
+                onPressed: _onChangeStatus,
+                child: const Text('Cambiar estado de la mesa'),
+              ),
+              const SafeArea(bottom: false, child: SizedBox.shrink()),
+            ],
+          ),
+        ),
       ),
     );
   }
