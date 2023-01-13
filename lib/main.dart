3aeaa5ae79252bba750d2951dcc9382b33d42fd0
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-13 09:33:20 -0500

    Added main logs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
@@ -7,18 +9,31 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:oyt_admin/app.dart';
 import 'package:oyt_admin/firebase_options.dart';
+import 'package:oyt_front_core/logger/logger.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  EquatableConfig.stringify = true;
-  initializeDateFormatting('es_CO', null);
-  if (kIsWeb) {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  } else {
-    await Firebase.initializeApp(
-      options: Firebase.apps.isNotEmpty ? DefaultFirebaseOptions.currentPlatform : null,
-    );
-  }
-  await Hive.initFlutter();
-  runApp(const ProviderScope(child: MyApp()));
+  runZonedGuarded<Future>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      EquatableConfig.stringify = true;
+      initializeDateFormatting('es_CO', null);
+      if (kIsWeb) {
+        await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+      } else {
+        await Firebase.initializeApp(
+          options: Firebase.apps.isNotEmpty ? DefaultFirebaseOptions.currentPlatform : null,
+        );
+      }
+      await Hive.initFlutter();
+      FlutterError.onError = (FlutterErrorDetails details) {
+        Logger.log(details.exceptionAsString());
+        Logger.log(details.stack.toString());
+      };
+      runApp(const ProviderScope(child: MyApp()));
+    },
+    (error, stackTrace) {
+      Logger.log(error.toString());
+      Logger.log(stackTrace.toString());
+    },
+  );
 }
