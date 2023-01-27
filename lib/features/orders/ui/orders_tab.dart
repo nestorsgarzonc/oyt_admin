c9b2a06803557b410809ddcf619db44072cd81d3
jubedoyat <jubedoyat@unal.edu.co>
2023-01-26 22:56:34 -0500

    PR corrections

--- lib/features/orders/ui/orders_tab.dart
+++ lib/features/orders/ui/orders_tab.dart
@@ -18,58 +18,60 @@ class _OrdersTabState extends ConsumerState<OrdersQueueTab> {
 
   @override
   Widget build(BuildContext context) {
+    final scrollController = ScrollController();
     final ordersQueueState = ref.watch(ordersQueueProvider);
-    return SafeArea(
-      child: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        children: [
-          const TabHeader(
-            title: 'Cola de productos',
-            subtitle:
-                'Acá puedes ver la cola de los productos y manejar los estados de estos.',
-          ),
-          const SizedBox(height: 14),
-          const Text(
-            'Filtrar por:',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-          ),
-          const SizedBox(height: 5),
-          CustomDropdownField<OrderStatus>(
-            items: OrderStatus.values,
-            value: _selectedStatus,
-            itemAsString: (item) => item.label,
-            onChanged: (value) => setState(() => _selectedStatus = value),
-            labelText: 'Estado del producto',
-            hintText: 'Selecciona un estado',
-          ),
-          const SizedBox(height: 5),
-          const Divider(),
-          const SizedBox(height: 5),
-          ordersQueueState.ordersQueue.on(
-            onError: (err) => Center(child: Text(err.message)),
-            onLoading: () => const LoadingWidget(),
-            onInitial: () => const Center(child: Text('No hay productos en cola')),
-            onData: (data) => data.isEmpty
-                ? const Center(child: Text('No hay productos en cola'))
-                : Column(
-                    children: data
-                        .map(
-                          (e) => Card(
-                            child: ListTile(
-                              contentPadding:
-                                  const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                              horizontalTitleGap: 10,
-                              title: Text('Producto: ${e.productName}'),
-                              subtitle: Text('Mesa: ${e.tableName}'),
-                              trailing: Text('Estado: \n${e.estado}'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const TabHeader(
+          title: 'Cola de productos',
+          subtitle:
+              'Acá puedes ver la cola de los productos y manejar los estados de los productos.',
+        ),
+        const SizedBox(height: 14),
+        const Text(
+          'Filtrar por:',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 5),
+        CustomDropdownField<OrderStatus>(
+          items: OrderStatus.values,
+          value: _selectedStatus,
+          itemAsString: (item) => item.label,
+          onChanged: (value) => setState(() => _selectedStatus = value),
+          labelText: 'Estado del producto',
+          hintText: 'Selecciona un estado',
+        ),
+        const Divider(),
+        Expanded(
+          child: Scrollbar(
+            controller: scrollController,
+            child: ordersQueueState.ordersQueue.on(
+              onError: (err) => Center(child: Text(err.message)),
+              onLoading: () => const LoadingWidget(),
+              onInitial: () => const Center(child: Text('No hay productos en cola')),
+              onData: (data) => data.isEmpty
+                  ? const Center(child: Text('No hay productos en cola'))
+                  : ListView(
+                      children: data
+                          .map(
+                            (e) => Card(
+                              child: ListTile(
+                                contentPadding:
+                                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                                horizontalTitleGap: 10,
+                                title: Text('Producto: ${e.productName}'),
+                                subtitle: Text('Mesa: ${e.tableName}'),
+                                trailing: Text('Estado: \n${e.estado}'),
+                              ),
                             ),
-                          ),
-                        )
-                        .toList(),
-                  ),
+                          )
+                          .toList(),
+                    ),
+            ),
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
